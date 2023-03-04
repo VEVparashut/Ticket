1c338db7c6ae0fdf693268c17c286ff199a41575
Neto test <89373786148@yandex.ru>
2023-03-04 17:41:30 +0300

    3th commit

--- src/test/java/TicketManagerTest.java
+++ src/test/java/TicketManagerTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 public class TicketManagerTest {
     TicketRepository repository = new TicketRepository();
     TicketManager manager = new TicketManager(repository);
@@ -9,12 +10,14 @@ public class TicketManagerTest {
     Ticket ticket1 = new Ticket(1, 48000, "VKO", "OGZ");
     Ticket ticket2 = new Ticket(2, 20000, "DME", "LED");
     Ticket ticket3 = new Ticket(3, 35500, "GOJ", "EGO");
+    Ticket ticket4 = new Ticket(4, 31000, "DME", "LED");
 
     @BeforeEach
     public void setup() {
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
+        manager.add(ticket4);
     }
 
     @Test
@@ -22,6 +25,24 @@ public class TicketManagerTest {
 
         Ticket[] expected = {ticket1};
         Ticket[] actual = manager.findAll("VKO", "OGZ");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllNullTest() {
+
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("DME", "VKO");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllTwoTest() {
+
+        Ticket[] expected = {ticket2, ticket4};
+        Ticket[] actual = manager.findAll("DME", "LED");
 
         Assertions.assertArrayEquals(expected, actual);
     }
